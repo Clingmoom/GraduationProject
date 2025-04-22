854aebb2082f5deb6b6553758d3c4620903661e8
Clingmoon <2081952862@qq.com>
2025-04-22 21:22:58 +0800

    feat(trainers): 添加AestheticMlp模型实现

---
+++ src/trainers/aesthetic_mlp.py
@@ -0,0 +1,27 @@
+import pytorch_lightning as pl
+from torch import nn
+
+
+class AestheticMlp(pl.LightningModule):
+    def __init__(self, input_size, xcol="emb", ycol="avg_rating"):
+        super().__init__()
+        self.input_size = input_size
+        self.xcol = xcol
+        self.ycol = ycol
+        self.layers = nn.Sequential(
+            nn.Linear(self.input_size, 1024),
+            # nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(1024, 128),
+            # nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(128, 64),
+            # nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(64, 16),
+            # nn.ReLU(),
+            nn.Linear(16, 1),
+        )
+
+    def forward(self, x):
+        return self.layers(x)
