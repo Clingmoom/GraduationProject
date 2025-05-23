5f2d5c77eca03402e8e65341321b3b237fd2bfb6
Clingmoon <2081952862@qq.com>
2025-05-23 16:28:19 +0800

    refactor(evaluate_pickscore): 优化pick_scores逻辑

--- src/evaluate_pickscore.py
+++ src/evaluate_pickscore.py
@@ -409,22 +409,9 @@ def main():
                 short_prompt_index = 0
                 eval_model_index = 1
 
-                all_images = []
-                for img, dy_img in zip(images, dy_images):
-                    all_images.extend([img, dy_img])
-                all_prompts = []
-                for p in prompt:
-                    all_prompts.extend([p] * 2)
-
-                pick_scores_raw = scorer.get_pick_score(all_prompts, all_images)
-
-                # pick_scores = torch.Tensor(
-                #     [scorer.get_pick_score(prompt, [img, dy_img])[eval_model_index] for img, dy_img in zip(images, dy_images)]
-                # )
-
-                pick_scores = torch.Tensor([
-                    pick_scores_raw[i + eval_model_index] for i in range(0, len(pick_scores_raw), 2)
-                ])
+                pick_scores = torch.Tensor(
+                    [scorer.get_pick_score(prompt, [img, dy_img])[eval_model_index] for prompt, img, dy_img in zip(plain_texts,images, dy_images)]
+                )
 
                 print(f"pick_scores:{pick_scores}")
                 pick_scores_sum += torch.Tensor(pick_scores).sum()
